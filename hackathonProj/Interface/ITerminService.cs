ec269d24c9c1dc33f8d4b4d719eb2fa2e96d2d96
egortar <egor.tarkovskij@gmail.com>
2018-06-15 23:34:19 +0200

    123

--- hackathonProj/Interface/ITerminService.cs
+++ hackathonProj/Interface/ITerminService.cs
@@ -1,6 +1,18 @@
-﻿namespace hackathonProj.Interface
+﻿using System.Collections.Generic;
+using hackathonProj.Model.Entities;
+using hackathonProj.Model.SearchCriterias;
+
+namespace hackathonProj.Interface
 {
     interface ITerminService
     {
+      bool AddTermin(Termin termin);
+      bool UpdateTermin(Termin termin);
+      bool DeleteTermin(int? terminId);
+      Termin GetTermin(int? terminId);
+      IList<Termin> GetTerminList(TerminSC accountSc);
+      Account GetAccount(int? terminId);
+      Wydzial GetWydzial(int? terminId);
+      Karta GetKarta(int? terminId);
     }
 }
--- hackathonProj/Model/Services/TerminService.cs
+++ hackathonProj/Model/Services/TerminService.cs
@@ -1,8 +1,58 @@
-﻿using hackathonProj.Interface;
+﻿using System.Collections.Generic;
+using hackathonProj.Extensions;
+using hackathonProj.Interface;
+using hackathonProj.Model.Entities;
+using hackathonProj.Model.SearchCriterias;
 
 namespace hackathonProj.Model.Services
 {
-    public class TerminService : ITerminService
+  public class TerminService : ITerminService
+  {
+    public bool AddTermin(Termin termin)
     {
+      return TerminDAO.Add(termin);
     }
+
+    public bool UpdateTermin(Termin termin)
+    {
+      return TerminDAO.Update(termin);
+    }
+
+    public bool DeleteTermin(int? terminId)
+    {
+      return TerminDAO.Delete(terminId);
+    }
+
+    public Termin GetTermin(int? terminId)
+    {
+      return TerminDAO.Get(terminId);
+    }
+
+    public IList<Termin> GetTerminList(TerminSC accountSc)
+    {
+      //TODO: Search criteria
+      return TerminDAO.GetList();
+    }
+
+    public Account GetAccount(int? terminId)
+    {
+      var accountId = TerminDAO.Get(terminId)?.AccountId;
+      var account = AccountDAO.Get(accountId);
+      return account.IsNotNull() ? account : new Account();
+    }
+
+    public Wydzial GetWydzial(int? terminId)
+    {
+      var wydzialId = TerminDAO.Get(terminId)?.AccountId;
+      var wydzial = WydzialDAO.Get(wydzialId);
+      return wydzial.IsNotNull() ? wydzial : new Wydzial();
+    }
+
+    public Karta GetKarta(int? terminId)
+    {
+      var kartaId = TerminDAO.Get(terminId)?.AccountId;
+      var karta = KartaDAO.Get(kartaId);
+      return karta.IsNotNull() ? karta : new Karta();
+    }
+  }
 }
